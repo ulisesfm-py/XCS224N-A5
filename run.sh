af140d28d14b97539037c1b5965f088d556bcc37
scpd-proed <haraguchi@stanford.edu>
2019-10-30 22:43:08 +0530

    enabling local training

--- run.sh
+++ run.sh
@@ -2,11 +2,22 @@
 
 if [ "$1" = "train" ]; then
 	CUDA_VISIBLE_DEVICES=0 python run.py train --train-src=./en_es_data/train.es --train-tgt=./en_es_data/train.en \
-        --dev-src=./en_es_data/dev.es --dev-tgt=./en_es_data/dev.en --vocab=vocab.json --cuda
+        --dev-src=./en_es_data/dev.es --dev-tgt=./en_es_data/dev.en --vocab=vocab.json --cuda --batch-size=2
+
 elif [ "$1" = "test" ]; then
     mkdir -p outputs
     touch outputs/test_outputs.txt
-    CUDA_VISIBLE_DEVICES=0 python run.py decode model.bin ./en_es_data/test.es ./en_es_data/test.en outputs/test_outputs.txt --cuda
+    CUDA_VISIBLE_DEVICES=0 python run.py decode model.bin ./en_es_data/test.es outputs/test_outputs.txt --cuda
+
+elif [ "$1" = "train_local" ]; then
+  	python run.py train --train-src=./en_es_data/train.es --train-tgt=./en_es_data/train.en \
+        --dev-src=./en_es_data/dev.es --dev-tgt=./en_es_data/dev.en --vocab=vocab.json --batch-size=2
+
+elif [ "$1" = "test_local" ]; then
+    mkdir -p outputs
+    touch outputs/test_outputs.txt
+    python run.py decode model.bin ./en_es_data/grader.es outputs/test_outputs.txt
+
 elif [ "$1" = "train_local_q1" ]; then
 	python run.py train --train-src=./en_es_data/train_tiny.es --train-tgt=./en_es_data/train_tiny.en \
         --dev-src=./en_es_data/dev_tiny.es --dev-tgt=./en_es_data/dev_tiny.en --vocab=vocab_tiny_q1.json --batch-size=2 \
